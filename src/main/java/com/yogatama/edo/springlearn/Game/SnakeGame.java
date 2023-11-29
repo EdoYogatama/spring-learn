0a366907b537360beb9be31efb53f9b849695d76
EdoYogatama <roronoadezoro@gmail.com>
2023-11-29 11:01:23 +0700

    code other games

--- src/main/java/com/yogatama/edo/springlearn/Game/SnakeGame.java
+++ src/main/java/com/yogatama/edo/springlearn/Game/SnakeGame.java
@@ -2,4 +2,33 @@ package com.yogatama.edo.springlearn.Game;
 
 public class SnakeGame {
 
+	private boolean isTurned = false;
+	
+	public void up() {
+		if(isTurned) {
+			System.out.println("Go Up");
+			isTurned = false;
+		} else {
+			System.out.println("Not Move");
+		}
+	}
+	
+	public void down() {
+		if(isTurned) {
+			System.out.println("Go Down");
+			isTurned = false;
+		} else {
+			System.out.println("Not Move");
+		}
+	}
+	
+	public void left() {
+		System.out.println("Turn Left");
+		isTurned = true;
+	}
+	
+	public void right() {
+		System.out.println("Turn Right");
+		isTurned = true;
+	}
 }
